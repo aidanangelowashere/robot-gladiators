6ff28c26e8a59da4f60e197955e54cd3965d9a54
aidan pc <aidanangelowashere@gmail.com>
2021-09-27 17:37:53 -0500

    robot name funcion, console log notes

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of Quotation marks around playerName
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// This Will Do math and log 20 
+console.log(10+10);
+// What is this?
+console.log ("Our robots name is " + playerName)
+function fight (){
+    window.alert("The fight has begun");
+}
+// fight();
